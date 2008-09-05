7956144f5a1871ab5e41bcb206d6475fe3e800ec
Sean Cribbs <seancribbs@gmail.com>
2008-09-05 11:23:54 -0400

    Improve documentation a little.

--- lib/templates/tags.rb
+++ lib/templates/tags.rb
@@ -5,6 +5,15 @@ module Templates::Tags
     tag.expand
   end
   
+  desc %{
+    Renders its contents if the selected part contains the text "true".  When
+    used with check_box part-types in a template, can turn on and off sections
+    of content.  The 'part' attribute is required.
+    
+    *Usage*:
+    
+    <pre><code><r:boolean_part:if part="display_sidebar">Sidebar content</r:boolean_part:if></code></pre>
+  }
   tag 'boolean_part:if' do |tag|
     part_name = tag.attr['part']
     raise StandardTags::TagError.new("`boolean_part:if' tag requires a 'part' attribute") unless part_name
@@ -13,6 +22,15 @@ module Templates::Tags
     tag.expand if result =~ /t|true|1/i
   end
   
+  desc %{
+    Renders its contents if the selected part does not contain the text "true".  When
+    used with check_box part-types in a template, can turn on and off sections
+    of content.  The 'part' attribute is required.
+    
+    *Usage*:
+    
+    <pre><code><r:boolean_part:unless part="display_sidebar">Sidebar content</r:boolean_part:unless></code></pre>
+  }
   tag 'boolean_part:unless' do |tag|
     part_name = tag.attr['part']
     raise StandardTags::TagError.new("`boolean_part:unless' tag requires a 'part' attribute") unless part_name
